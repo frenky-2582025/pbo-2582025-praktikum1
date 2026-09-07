15b8c9d8395cc6b80239578c239fa454ac772042
frenky-2582025 <2582025@unai.edu>
2026-09-07 10:34:43 +0700

    Input data diri

--- src/BiodataDiri.java
+++ src/BiodataDiri.java
@@ -3,5 +3,25 @@ import java.util.Scanner;
 public class BiodataDiri {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan Nama: ");
+        String nama = scanner.nextLine();
+
+        // Menggunakan String agar NIM panjang tidak error
+        System.out.print("Masukkan NIM: ");
+        String nim = scanner.nextLine();
+
+        System.out.print("Masukkan Umur: ");
+        int umur = scanner.nextInt();
+
+        System.out.print("Masukkan Tinggi (cm): ");
+        int tinggi = scanner.nextInt();
+
+        // Membersihkan karakter newline tersisa sebelum membaca String lagi
+        scanner.nextLine();
+
+        System.out.print("Masukkan Asal Kota: ");
+        String asalKota = scanner.nextLine();
+
     }
 }
