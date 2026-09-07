92e3508563704dfdd203c6a0f73d923664b5d552
frenky-2582025 <2582025@unai.edu>
2026-09-07 10:33:40 +0700

    Masukan scanner

--- src/BiodataDiri.java
+++ src/BiodataDiri.java
@@ -1,14 +1,7 @@
+import java.util.Scanner;
+
 public class BiodataDiri {
-    public static void main(String[] args ){
-        String nama = "Frenky jul prima jaya waruwu";
-        String nim = "2582025";
-        String asalDaerah = "Nias";
-        String alasanmemilihProdi = "karena prospek pekerjaan yang saya suka sesuai dan juga prospek perkerjaannya luas";
-        String targetsemesterIni = "Ingin menguasai bahasa pemograma java";
-        System.out.println("Nama                    : " + nama);
-        System.out.println("Nim                     : " + nim);
-        System.out.println("Asal Daerah             : " + asalDaerah);
-        System.out.println("Alasan Memilih Prodi    : " + alasanmemilihProdi);
-        System.out.println("Target Semester Ini     : " + targetsemesterIni);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
     }
 }
